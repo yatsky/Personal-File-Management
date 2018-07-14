ceece6f6fa08da3dd0ca00cc3bfed1168852e172
Yi Wang <y.wang7@uqconnect.edu.au>
2018-07-14 22:15:11 +1000

    Init upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env
+
+# This is the program that is used to manage my video data locally
+# potentially this could also be used to manage all other files
+# Expected Functions include:
+#   1. Detecting files that have the same size in bytes
+#   2. Detecting files with the same name (exact match except for the extension)
+#   3. Listing all files found in 1 and 2 and store them in a csv file with an 
+#       absolute path to the files
+#   4. Allow manually deleting entries in the csv file created in 3 and is able 
+#       to read the csv file and delete files listed according to 5
+#   5. For files with same size, keep file that has a human-readable name (this is
+#       sometimes a manual process); for files with same name, keep file that is
+#       larger (assumed to be higher quality)
+# 
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
