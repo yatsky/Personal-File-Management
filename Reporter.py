42a3196c6306af3dc177c7e5e6b5325911de6c6f
Thomas Wang <wyatsky@gmail.com>
2018-07-15 15:06:06 +1000

    Update clean file name condition, only [^\w\n\s\.]

--- Reporter.py
+++ Reporter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from os import path, rmdir, remove, walk
+from os import path, rmdir, remove, walk, rename
 import re
 
 
@@ -27,8 +27,10 @@ class Reporter():
         for root, dirs, filenames in walk(self.root):
             for filename in filenames:
                 # TODO Need to deal with file names that have comma in them
-                self.index["filename"].append(filename.replace(",",""))
-                file_path = path.join(root, filename)
+                newname = self._clean_filename(filename)
+                self._rename_file(newname,filename,root)
+                self.index["filename"].append(newname)
+                file_path = path.join(root, newname)
                 self.index["path"].append(file_path)
                 self.index["size"].append(path.getsize(file_path))
                 self.index["create_date"].append(path.getmtime(file_path))
@@ -43,6 +45,10 @@ class Reporter():
                     continue 
     @staticmethod
     def _clean_filename(filename):
-       pattern = re.compile("[^\w\n\s]")
+       pattern = re.compile(r"[^\w\n\s\.]")
        newname = re.sub(pattern, "_", filename)
        return newname
+
+    @staticmethod
+    def _rename_file(newname, filename, root):
+        rename(path.join(root, filename), path.join(root, newname))
